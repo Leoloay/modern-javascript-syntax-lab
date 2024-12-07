7010d52d7748ca96b7fd824991295cd1ced360d9
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-12-07 13:06:12 +0300

    Last Commit

--- index.js
+++ index.js
@@ -87,5 +87,79 @@ const userProfile = {
 console.log(userProfile)
 
 //Ex 7
+//Done
 
+//Ex 8
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
 
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+
+const nounAndAdj = (noun, adjective) => {
+  return console.log(`The ${noun} is ${adjective}`)
+}
+
+nounAndAdj("cat", "white")
+
+//Ex 9
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = "tasty"
+
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
+
+// Your code here
+let tastyOrNot = pizza === "tasty" ? "yum" : "yuck"
+
+console.log(tastyOrNot)
+
+//Ex 10
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement:
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+const LANG = localLangConfig || "en"
+
+// Log the result
+console.log("Language setting:", LANG)
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || "light"
+
+// Log the result
+console.log("User theme setting:", USER_THEME)
+
+//Ex 11
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+  name: "Alice",
+}
+
+let cat = adventurer.cat?.age // Your code here
+
+console.log(cat)
